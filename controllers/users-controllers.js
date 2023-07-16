ae7c245e0cc36f8fa1d1fafab622014ce52349bf
Taylor Lo <taylorloph@gmail.com>
2023-07-15 23:23:02 -0400

    Added change-info patch route

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -31,5 +31,44 @@ const getUserById = async (req, res, next) => {
   res.json({ user: user.toObject({ getters: true }) });
 };
 
+const changeUserInfo = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new HttpError('Invalid inputs passed, please check your data.', 422));
+  }
+  const { userName, url } = req.body;
+  const userId = req.session;
+  console.log(req.session);
+
+  let user;
+  try {
+    user = await User.findById(userId);
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not update user', 500);
+    return next(error);
+  }
+
+  if (user._id.toString() !== userId) {
+    const error = new HttpError('You are not allowed to change the info of this user.', 401);
+    return next(error);
+  } else if (user.userName === userName) {
+    const error = new HttpError('This username have been used.', 401);
+    return next(error);
+  }
+
+  user.userName = userName;
+  user.channelURL = url;
+
+  try {
+    await user.save();
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not update user.', 500);
+    return next(error);
+  }
+
+  res.status(200).json({ user: user.toObject({ getters: true }) });
+};
+
 exports.getUsers = getUsers;
 exports.getUserById = getUserById;
+exports.changeUserInfo = changeUserInfo;
