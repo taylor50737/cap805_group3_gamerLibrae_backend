7583a677176d236851a1852553fd01b313c80032
Taylor Lo <taylorloph@gmail.com>
2023-07-28 13:21:37 -0400

    Updated change password route + controller

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -10,7 +10,7 @@ router.get('/', usersController.getUsers);
 
 router.get('/:uid', usersController.getUserById);
 
-router.get('/wishlist/:uid', usersController.getWishListByUserId)
+router.get('/wishlist/:uid', usersController.getWishListByUserId);
 
 router.patch(
   '/change-info',
@@ -19,6 +19,13 @@ router.patch(
   usersController.changeUserInfo,
 );
 
-
+router.patch('/change-password', 
+// authMiddleware.loginRequired, 
+[
+  check('currentPassword').not().isEmpty(),
+  check('newPassword').isLength({ min: 5 }),
+  check('confirmPassword').isLength({ min: 5 }),
+  usersController.changePassword,
+]);
 
 module.exports = router;
